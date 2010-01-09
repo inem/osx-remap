056bee34dc28e3806deab3f38095d536846d6798
coderrr <coderrr.contact@gmail.com>
2010-01-10 06:48:49 +0700

    display event flags

--- remap.c
+++ remap.c
@@ -1,7 +1,39 @@
 #include <Carbon/Carbon.h>
 
+struct flag_mapping {
+  CGEventFlags bit;
+  char *name;
+};
+
+const struct flag_mapping event_flags[] = {
+  { kCGEventFlagMaskAlphaShift, "alpha-shift" },
+  { kCGEventFlagMaskShift, "shift" },
+  { kCGEventFlagMaskControl, "ctrl" }, 
+  { kCGEventFlagMaskAlternate, "alt" },
+  { kCGEventFlagMaskCommand, "cmd" },
+  /* Special key identifiers. */
+  { kCGEventFlagMaskHelp, "help" },
+  { kCGEventFlagMaskSecondaryFn, "fn" }, 
+  /* Identifies key events from numeric keypad area on extended keyboards. */
+  { kCGEventFlagMaskNumericPad, "numpad" },
+  /* Indicates if mouse/pen movement events are not being coalesced */
+  { kCGEventFlagMaskNonCoalesced, "noncoalesced" },
+  {0,0}
+};
+
 CGEventRef event_handler(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *data) {
-  printf(".");fflush(stdout);
+  CGKeyCode keycode = (CGKeyCode)CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
+  CGEventFlags flagbits = CGEventGetFlags(event);
+  char flags[1024] = {0};
+  int i;
+
+  for (i = 0; event_flags[i].name; i++) {
+    if (flagbits & event_flags[i].bit) {
+      strcat(flags, event_flags[i].name); strcat(flags, " ");
+    }
+  }
+  printf("%x flg: %s\n", keycode, flags);
+
   return event;
 }
 
