ff142acd3ab27f43a94a7522ff76565fd02b741d
coderrr <coderrr.contact@gmail.com>
2010-01-10 10:33:17 +0700

    #defines for cleaner code; remaps to get closer to my linux setup

--- remap.c
+++ remap.c
@@ -5,6 +5,14 @@ struct flag_mapping {
   char *name;
 };
 
+#define ALT kCGEventFlagMaskAlternate
+#define CMD kCGEventFlagMaskCommand
+#define CTRL kCGEventFlagMaskControl
+#define FN kCGEventFlagMaskSecondaryFn
+#define NUM kCGEventFlagMaskNumericPad
+#define SHIFT kCGEventFlagMaskShift
+#define ASHIFT kCGEventFlagMaskAlphaShift
+
 const struct flag_mapping event_flags[] = {
   { kCGEventFlagMaskAlphaShift, "alpha-shift" },
   { kCGEventFlagMaskShift, "shift" },
@@ -42,18 +50,42 @@ char *key_to_str(CGKeyCode keycode, CGEventFlags flags) {
 }
 
 void remap_keys(CGEventRef event, CGKeyCode keycode, CGEventFlags flags) {
-  if (keycode == 0x30 && flags & kCGEventFlagMaskCommand) {
-    flags &= ~kCGEventFlagMaskCommand;
-    flags |= kCGEventFlagMaskAlternate;
+  if (keycode == 0x30 && flags & CMD) {
+    flags &= ~CMD;
+    flags |= ALT;
   }
 
-  if (flags & kCGEventFlagMaskControl) {
-    flags &= ~kCGEventFlagMaskControl;
-    flags |= kCGEventFlagMaskSecondaryFn;
-  } else if (flags & kCGEventFlagMaskSecondaryFn && ! (flags & kCGEventFlagMaskNumericPad)) {
-    flags &= ~kCGEventFlagMaskSecondaryFn;
-    flags |= kCGEventFlagMaskControl;
+  CGEventFlags flagscopy = flags & (ALT|CMD|CTRL|FN|NUM|SHIFT|ASHIFT);
+
+  // arrows
+  if (keycode >= 0x7b && keycode <= 0x7e && flagscopy & (NUM|FN)) {
+    // change spaces with cmd+ctrl+arrows instead of just cmd+arrows
+    if (flagscopy == (CMD|CTRL|NUM|FN)) {
+      flags &= ~CTRL;
+    }
+    // move win to diff space with shift+cmd+ctrl+arrows instead of just cmd+ctrl+arrows
+    if (flagscopy == (CMD|CTRL|SHIFT|NUM|FN))
+      flags &= ~SHIFT;
   }
+
+  /* this has weird side effects, braindump:
+
+     like hjitting ctrl-delete wchicj should now be fn-delete, didnt actually do the normal delete , it did a bakspace still
+     like it turns out the fn key actually changes the keycode as well
+     usually the delete key is 33
+     but it returns 75 when u hit it with fn
+     same with fn and the number pad
+     and the thing that allows me tomove windows with ctrl-cmd-drag
+     it doesnt work with my remap of fn to ctrl
+
+  if (flags & CTRL) {
+    flags &= ~CTRL;
+    flags |= FN;
+  } else if (flags & FN && ! (flags & NUM)) {
+    flags &= ~FN;
+    flags |= CTRL;
+  }
+  */
 
   printf("%s\n", key_to_str(keycode, flags));
 
