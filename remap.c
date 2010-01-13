48d41e07f3fd1b94c2f36e706f71e60d392df996
coderrr <coderrr.contact@gmail.com>
2010-01-13 07:37:10 +0700

    move remap logic to ruby remap server; add force quit flag to make sure we exit if loop doesnt stop

--- remap.c
+++ remap.c
@@ -1,5 +1,8 @@
 #include <Carbon/Carbon.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <sys/time.h>
+#include <netinet/in.h>
 
 struct timeval last_event_1 = { 0, 0 }, last_event_2 = { 0, 0 };
 
@@ -8,113 +11,55 @@ struct flag_mapping {
   char *name;
 };
 
-#define ALT kCGEventFlagMaskAlternate
-#define CMD kCGEventFlagMaskCommand
-#define CTRL kCGEventFlagMaskControl
-#define FN kCGEventFlagMaskSecondaryFn
-#define NUM kCGEventFlagMaskNumericPad
-#define SHIFT kCGEventFlagMaskShift
-#define ASHIFT kCGEventFlagMaskAlphaShift
-
-const struct flag_mapping event_flags[] = {
-  { kCGEventFlagMaskAlphaShift, "alpha-shift" },
-  { kCGEventFlagMaskShift, "shift" },
-  { kCGEventFlagMaskControl, "ctrl" }, 
-  { kCGEventFlagMaskAlternate, "alt" },
-  { kCGEventFlagMaskCommand, "cmd" },
-  /* Special key identifiers. */
-  { kCGEventFlagMaskHelp, "help" },
-  { kCGEventFlagMaskSecondaryFn, "fn" }, 
-  /* Identifies key events from numeric keypad area on extended keyboards. */
-  { kCGEventFlagMaskNumericPad, "numpad" },
-//  /* Indicates if mouse/pen movement events are not being coalesced */
-//  { kCGEventFlagMaskNonCoalesced, "noncoalesced" },
-  {0,0}
-};
-
 void stop_loop() {
   CFRunLoopStop(CFRunLoopGetCurrent());
 }
 
-char *key_to_str(CGKeyCode keycode, CGEventFlags flags) {
-  static char str[1024];
-  char flags_str[1024] = {0};
-  int i;
-
-  for (i = 0; event_flags[i].name; i++) {
-    if (flags & event_flags[i].bit) {
-      strcat(flags_str, event_flags[i].name); strcat(flags_str, "-");
-    }
-  }
-
-  sprintf(str, "%s%x", flags_str, keycode);
-
-  return str;
-}
-
 void remap_keys(CGEventRef event, CGKeyCode keycode, CGEventFlags flags) {
-  if (keycode == 0x30 && flags & CMD) {
-    flags &= ~CMD;
-    flags |= ALT;
+  CGKeyCode new_keycode;
+  CGEventFlags new_flags;
+  char tmp[1024];
+
+  struct sockaddr_in sin = { .sin_family = AF_INET, .sin_port = htons(9999), .sin_addr = { .s_addr = inet_addr("127.0.0.1") } };
+  int s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP), ret, len = 0;
+  connect(s, &sin, sizeof(sin));
+
+  sprintf(tmp, "%hu %lu\n", keycode, flags);
+  ret = write(s, tmp, strlen(tmp));
+
+  memset(tmp, 0, sizeof(tmp));
+  while (ret > 0 && !strstr(tmp, "\n")) {
+    ret = read(s, &tmp[len], sizeof(tmp)-len);
+    len += ret;
   }
+  tmp[len] = 0;
 
-  CGEventFlags flagscopy = flags & (ALT|CMD|CTRL|FN|NUM|SHIFT|ASHIFT);
+  close(s);
 
-  // arrows
-  if (keycode >= 0x7b && keycode <= 0x7e && flagscopy & (NUM|FN)) {
-    // map cmd-arrow to alt-*
-    if (flagscopy == (CMD|NUM|FN)) {
-      flags &= ~CMD;
-      flags |= ALT;
-    }
-
-    // change spaces with cmd+ctrl+arrows instead of just cmd+arrows
-    if (flagscopy == (CMD|CTRL|NUM|FN))
-      flags &= ~CTRL;
-
-    // move win to diff space with shift+cmd+ctrl+arrows instead of just cmd+ctrl+arrows
-    if (flagscopy == (CMD|CTRL|SHIFT|NUM|FN))
-      flags &= ~SHIFT;
+  if (ret > 0) {
+    sscanf(tmp, "%hu %lu\n", &new_keycode, &new_flags);
+    printf("[%hu/%lu] -> [%hu/%lu]\n", keycode, flags, new_keycode, new_flags);
+    CGEventSetFlags(event, new_flags);
   }
-
-  // cmd-backspace to alt-backspace
-  if (keycode == 0x33 && flagscopy == CMD) {
-    flags &= ~CMD;
-    flags |= ALT;
-  }
-
-
-  /* this has weird side effects, braindump:
-
-     like hjitting ctrl-delete wchicj should now be fn-delete, didnt actually do the normal delete , it did a bakspace still
-     like it turns out the fn key actually changes the keycode as well
-     usually the delete key is 33
-     but it returns 75 when u hit it with fn
-     same with fn and the number pad
-     and the thing that allows me tomove windows with ctrl-cmd-drag
-     it doesnt work with my remap of fn to ctrl
-
-  if (flags & CTRL) {
-    flags &= ~CTRL;
-    flags |= FN;
-  } else if (flags & FN && ! (flags & NUM)) {
-    flags &= ~FN;
-    flags |= CTRL;
-  }
-  */
-
-  printf("%s\n", key_to_str(keycode, flags));
-
-  CGEventSetFlags(event, flags);
 }
 
+bool force_exit = 0;
 void check_times() {
+  if (force_exit) {
+    printf("forced exit...\n");
+    exit(1);
+  }
+
   long double t1 = last_event_1.tv_sec + ((long double)last_event_1.tv_usec/(long double)1000000.0);
   long double t2 = last_event_2.tv_sec + ((long double)last_event_2.tv_usec/(long double)1000000.0);
 
   printf("time diff %Lf\n", fabsl(t1-t2));
   // more than a 0.1 second delay between events, highly likely that one listener is dead
-  if (fabsl(t1 - t2) > 0.1) stop_loop();
+  if (fabsl(t1 - t2) > 0.1) {
+    stop_loop();
+    // force exit on next timer in case the stupid loop doesn't actually stop
+    force_exit = 1;
+  }
 }
 
 CGEventRef event_handler(CGEventTapProxy proxy, CGEventType ev_type, CGEventRef event, void *data) {
@@ -127,7 +72,6 @@ CGEventRef event_handler(CGEventTapProxy proxy, CGEventType ev_type, CGEventRef 
 
   if (!(source = CGEventCreateSourceFromEvent(event))) return event;
 
-  printf("%s -> ", key_to_str(keycode, flags));
   remap_keys(event, keycode, flags);
 
   CFRelease(source);
---
+++ remap.rb
@@ -0,0 +1,92 @@
+require 'gserver'
+
+class Fixnum
+  def has?(bit)
+    self & bit == bit
+  end
+end
+
+ASHIFT = 0x00010000
+SHIFT = 0x00020000
+CTRL = 0x00040000
+ALT = 0x00080000
+CMD = 0x00100000
+NUM = 0x00200000
+HELP = 0x00400000
+FN = 0x00800000
+NON = 0x00000100
+
+KEY_NAMES = [
+  [SHIFT, 'shift'],
+  [CTRL, 'crtl'],
+  [ALT, 'alt'],
+  [CMD, 'cmd'],
+  [FN, 'fn'],
+  [NUM, 'num']
+]
+
+
+class RemapServer < GServer
+  def initialize(port)
+    super(port, '127.0.0.1')
+  end
+
+  def serve(io)
+    keycode, flags = io.gets.split(/\s+/).map {|s| s.strip.to_i }
+
+    new_keycode, new_flags = remap_keys(keycode, flags)
+
+    puts "#{key_to_str(keycode, flags)} -> #{key_to_str(new_keycode, new_flags)} .. [#{keycode}/#{flags}] -> [#{new_keycode}/#{new_flags}]"
+    io.puts "#{new_keycode} #{new_flags}"
+  end
+
+  def remap_keys(keycode, oflags)
+    key_bits = KEY_NAMES.map{|b,n| b }
+    flags = oflags & key_bits.inject {|bits, bit| bits | bit }
+
+    # cmd-tab -> alt-tab
+    if keycode == 0x30 and flags.has? CMD
+      oflags &= ~CMD
+      oflags |= ALT
+    end
+
+    # arrows
+    if (0x7b..0x7e).include? keycode and flags.has? NUM|FN
+      # map ctrl-(shift)-arrow -> alt-(shift)-arrow
+      if (flags&~SHIFT) == (CTRL|NUM|FN)
+        oflags &= ~CTRL
+        oflags |= ALT
+      end
+
+      # cmd-ctrl-arrows -> cmd-arrows (changign spaces)
+      if flags == CMD|CTRL|NUM|FN
+        oflags &= ~CTRL
+      end
+
+      # shift-cmd-ctrl-arrows -> cmd-ctrl-arrows (for moving windows between spaces)
+      if flags == CMD|CTRL|SHIFT|NUM|FN
+        oflags &= ~SHIFT
+      end
+    end
+
+    # ctrl-backspace -> alt-backspace
+    if keycode == 0x33 and flags == CTRL
+      oflags &= ~CTRL
+      oflags |= ALT
+    end
+
+    [keycode, oflags]
+  end
+
+  def key_to_str(keycode, flags)
+    s = ''
+    KEY_NAMES.map do |bit, name|
+      s << name << '-' if ! (flags & bit).zero?
+    end
+    s << keycode.to_s
+  end
+end
+
+s = RemapServer.new(9999)
+s.start
+s.join
